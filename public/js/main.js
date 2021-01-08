1a4b7a07a30c8ce421896236c1f11c81547aa967
Louise <louisepeiffer@hotmail.com>
2021-01-08 09:46:22 +0100

    fini

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+// Exo 1
+for (let index = 0; index < 20; index++) {
+    console.log("Bonjour");    
+}
+for (let index = 0; index < 21; index++) {
+    console.log(index);
+}
+
+// Exo 2
+let classe = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "maxence", "zulma"]
+
+for (let i = 0; i < classe.length; i++) {
+    console.log("Bonjour " + classe[i]);   
+}
+
+// Exo 3
+let nombre = parseInt(prompt('Un nombre'))
+console.log(nombre.length);
+for (let i = 0; i < nombre + 1; i++) {
+    console.log(i);
+}
